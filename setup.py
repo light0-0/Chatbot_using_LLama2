8dc28889069b7cbe4b05b714557f22295c2b8e30
Hardik <Bhagathardik1000@gmail.com>
2024-04-18 17:05:00 +0530

    setup file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot',
+    version= '0.0.0',
+    author= 'Hardik Ravi Bhagat',
+    author_email= 'Bhagathardik1000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
